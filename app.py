663685f8b9934f6f6ec315dd81d5d920c5816f0f
Anish Mishra <mishraanish8757@gmail.com>
2024-01-24 11:21:12 +0530

    Updated Code

--- app.py
+++ app.py
@@ -43,14 +43,14 @@ def input_pdf_setup(uploaded_file):
         raise FileNotFoundError("No file uploaded")
 
 ## Streamlit App
-st.set_page_config(page_title="ATS Resume Expert")
+st.set_page_config(page_title="Resume Reviewer",page_icon=":heavy_dollar_sign:")
 
 # Applying custom CSS
 with open('style.css') as f :
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-st.header("AI-Powered Resume Review & Analysis")
-st.subheader('Enhance Your Resume with AI Insights, ensuring it surpasses ATS screening in the competitive job market.')
+st.header("AI-Driven Resume Review & Analysis")
+st.subheader('Enhance Your Resume with AI Insights, ensuring it surpasses ATS screening in the your job search.')
 
 input_text = st.text_area("Paste Job Description here : ", key="input", placeholder="Enter Here")
 uploaded_file = st.file_uploader("Upload your Resume Here (in PDF Format Only) :", type=["pdf"])
@@ -63,22 +63,26 @@ col1, col2, col3 = st.columns([1,1,1])
 with col1:
     submit1 = st.button("Tell me about the Resume")
 with col2:
-    submit2 = st.button("How can I improve my skills")
+    submit2 = st.button("Check JD Match Percentage")
 with col3:
-    submit3 = st.button("Generate Match Percentage")
+    submit3 = st.button("How to Improve my Skills")
 
 col1, col2, col3 = st.columns([1,1,1])
 
 with col1:
     submit4 = st.button("Proofread & Edit Resume")
 with col2:
-    submit5 = st.button("Will be Used.")
+    submit5 = st.button("ATS Compatibility Check")
 with col3:
-    submit6 = st.button("Here you go")
+    submit6 = st.button("Measure SMART Impact")
 
+col4, col5 = st.columns([6.2,1])
 
-query_text = st.text_input("Ask your Custom Questions : ", key="input_question", placeholder="Type Here")
-search_btn = st.button("Search", type="primary")
+with col4:
+    query_text = st.text_input("Ask your Custom Questions : ", key="input_question", placeholder="Type Here")
+
+with col5:
+    search_btn = st.button("Search", type="primary")
 
 
 input_prompt1 = """
@@ -90,105 +94,120 @@ input_prompt1 = """
 """
 
 input_prompt2 = """
-You are an experienced Human Resource Manager with tech experience in the field of Software Development,
-Data Science, Full Stack Web Development, DEVOPS, Data Analyst and Data Engineering roles. 
-Please share your professional evaluation on how the candiate can improve his/her profile for the given role. 
-Highlight the areas of improvement for the applicant with corresponding to given resume and in relation to 
-the specified job requirements.
-"""
-
-input_prompt3 = """
 You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of Software Development,
-Data Science, Full Stack Web Development, DEVOPS, Data Analyst, Data Engineering and deep ATS functionality, 
-your task is to evaluate the resume against the provided job description. Give me the percentage of match if the 
+Data Science, Full Stack Web Development, DEVOPS, Data Analyst, Data Engineering and deep ATS functionality. 
+Your task is to evaluate the resume against the provided job description. Give me the percentage of match if the 
 resume matches the job description. First the output should come as percentage and then keywords missing and 
 last final thoughts.
 """
 
+input_prompt3 = """
+You are an experienced Human Resource Manager with tech experience in the field of Software Development,
+Data Science, Full Stack Web Development, DEVOPS, Data Analyst and Data Engineering roles. 
+Please share your professional evaluation on how the candidate can improve his/her profile for the given role. 
+Highlight the areas of improvement for the applicant with corresponding to given resume and in relation to 
+the specified job requirements.
+"""
+
 input_prompt4 = """
-You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and deep ATS functionality, 
-your task is to find any grammatical or spelling miskates in the resume. Also, Give me some highlights about the sentence structure 
-used in the resume and if it requires any changes. First the output should come about spelling and gramatical errors
-and then scope of improviment in the sentences used in resume and last final thoughts.
+You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of Software Development,
+Data Science, Full Stack Web Development, DEVOPS, Data Analyst, Data Engineering and deep ATS functionality. 
+Your task is to find any spelling errors, typos, grammatical errors and formatting issues miskates in the resume. 
+Also, Give me some highlights about the sentence structure used in the resume and if it requires any changes. 
+First the output should come about spelling and gramatical errors and then scope of improviment in the sentences used in resume and last final thoughts. Don't 
+give grammatical in the given prompts, use only resume.
 """
 
 input_prompt5 = """
-You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and deep ATS functionality, 
-your task is to find any grammatical or spelling miskates in the resume. Also, Give me some highlights about the sentence structure 
-used in the resume and if it requires any changes. First the output should come about spelling and gramatical errors
-and then scope of improviment in the sentences used in resume and last final thoughts.
+You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of Software Development,
+Data Science, Full Stack Web Development, DEVOPS, Data Analyst, Data Engineering and deep ATS functionality.  
+Your task is to evaluate the formatting and structure of the given resume. Highlight any potential issues that 
+might hinder ATS readability. Provide recommendations in well structured paragraphs for a clean and well-organized
+layout to ensure optimal parsing by Applicant Tracking Systems. Evaluate the overall compatibility of the given
+resume with common ATS platforms. Check for elements that might hinder parsing, and ensure adherence to ATS 
+best practices. Provide feedback on optimizing your resume to enhance its compatibility with Applicant Tracking 
+Systems.
 """
 
 input_prompt6 = """
-You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and deep ATS functionality, 
-your task is to find any grammatical or spelling miskates in the resume. Also, Give me some highlights about the sentence structure 
-used in the resume and if it requires any changes. First the output should come about spelling and gramatical errors
-and then scope of improviment in the sentences used in resume and last final thoughts.
+You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of Software Development,
+Data Science, Full Stack Web Development, DEVOPS, Data Analyst, Data Engineering and deep ATS functionality. 
+Your task is to determine that did the given resume effectively showcase his/her accomplishments and quantify 
+their impact? Were the achievements specific, measurable, achievable, relevant, and time-bound (SMART)? Give
+detailed feedback(in points) on areas for improvement for future job searches. Candidates appreciate constructive 
+criticism and the opportunity to learn from the experience.
 """
 
 if submit1:
     if uploaded_file is not None:
         pdf_content=input_pdf_setup(uploaded_file)
-        response=get_gemini_response(input_prompt1,pdf_content,input_text)
-        st.subheader("The Repsonse is")
-        st.write(response)
+        with st.spinner("Analyzing Your Resume"):
+            response=get_gemini_response(input_prompt1,pdf_content,input_text)
+            st.subheader("The Repsonse is")
+            st.write(response)
     else:
-        st.write("Please uplaod the resume")
+        st.write("Please upload the resume")
 
 elif submit2:
     if uploaded_file is not None:
         pdf_content=input_pdf_setup(uploaded_file)
-        response=get_gemini_response(input_prompt2,pdf_content,input_text)
-        st.subheader("The Repsonse is")
-        st.write(response)
+        with st.spinner("Analyzing Your Resume"):
+            response=get_gemini_response(input_prompt2,pdf_content,input_text)
+            st.subheader("The Repsonse is")
+            st.write(response)
     else:
-        st.write("Please uplaod the resume")
+        st.write("Please upload the resume")
 
 elif submit3:
     if uploaded_file is not None:
         pdf_content=input_pdf_setup(uploaded_file)
-        response=get_gemini_response(input_prompt3,pdf_content,input_text)
-        st.subheader("The Repsonse is")
-        st.write(response)
+        with st.spinner("Analyzing Your Resume"):
+            response=get_gemini_response(input_prompt3,pdf_content,input_text)
+            st.subheader("The Repsonse is")
+            st.write(response)
     else:
-        st.write("Please uplaod the resume")
+        st.write("Please upload the resume")
 
 elif submit4:
     if uploaded_file is not None:
         pdf_content=input_pdf_setup(uploaded_file)
-        response=get_gemini_response(input_prompt4,pdf_content,input_text)
-        st.subheader("The Repsonse is")
-        st.write(response)
+        with st.spinner("Analyzing Your Resume"):
+            response=get_gemini_response(input_prompt4,pdf_content,input_text)
+            st.subheader("The Repsonse is")
+            st.write(response)
     else:
-        st.write("Please uplaod the resume")
+        st.write("Please upload the resume")
 
 elif submit5:
     if uploaded_file is not None:
         pdf_content=input_pdf_setup(uploaded_file)
-        response=get_gemini_response(input_prompt5,pdf_content,input_text)
-        st.subheader("The Repsonse is")
-        st.write(response)
+        with st.spinner("Analyzing Your Resume"):
+            response=get_gemini_response(input_prompt5,pdf_content,input_text)
+            st.subheader("The Repsonse is")
+            st.write(response)
     else:
-        st.write("Please uplaod the resume")
+        st.write("Please upload the resume")
 
 elif submit6:
     if uploaded_file is not None:
         pdf_content=input_pdf_setup(uploaded_file)
-        response=get_gemini_response(input_prompt6,pdf_content,input_text)
-        st.subheader("The Repsonse is")
-        st.write(response)
+        with st.spinner("Analyzing Your Resume"):
+            response=get_gemini_response(input_prompt6,pdf_content,input_text)
+            st.subheader("The Repsonse is")
+            st.write(response)
     else:
-        st.write("Please uplaod the resume")
+        st.write("Please upload the resume")
 
 elif search_btn:
     if uploaded_file is not None:
         pdf_content=input_pdf_setup(uploaded_file)
-        response=get_gemini_response(query_text,pdf_content,input_text)
-        st.subheader("The Repsonse is")
-        st.write(response)
+        with st.spinner("Analyzing Your Resume"):
+            response=get_gemini_response(query_text,pdf_content,input_text)
+            st.subheader("The Repsonse is")
+            st.write(response)
     else:
-        st.write("Please uplaod the resume")
+        st.write("Please upload the resume")
 
 st.markdown("---")
-st.caption("Resume Reviewer - Making Job Hunting Easier")
-st.caption("Made by :orange_heart: - Anish")
+st.caption("Resume Reviewer - Make Job Search Easier")
+st.caption("Made with :orange_heart: - By Anish")
